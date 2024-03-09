8128d997b6f37a6b6c0870f8e4cc37019b618646
Sudaisneelambra <sudaisanuneelambra21@gmail.com>
2024-03-09 11:40:36 +0530

    complitted

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 require('dotenv').config()
 const cors = require('cors');
 const mongoose = require('mongoose');
-const chat = require('./models/chats');
-
-const tokens= require('./middlewares/tokenteck')
-
-
 const port = process.env.PORT;
 const DB_url =process.env.DB_connection
 
@@ -21,13 +16,7 @@ const io = require('socket.io')(httpServer, {
 // using cors
 app.use(cors());
 
-
-  const {postchat}=require('./controllers/chat.controller');
-  const {usermessages}=require('./controllers/chat.controller');
-  const {allusermessages}=require('./controllers/chat.controller');
-  const {getsingleusrchat}=require('./controllers/chat.controller');
-
-
+ const {postchat}=require('./controllers/chat.controller');
 
 
   io.on('connection', (socket) => {
@@ -35,7 +24,10 @@ app.use(cors());
     socket.on('message', (data) => {
      const {message, sender,reviver} =  data  
       postchat(message, sender,reviver)
-      io.emit('message', `${sender}: ${message}`);
+      data.date = new Date()
+      data.reciever = reviver
+      data.chatdata = message
+      io.emit('message', data);
     });
   
     socket.on('disconnect', () => {
@@ -47,13 +39,5 @@ app.use(cors());
 mongoose.connect(DB_url)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Failed to connect to MongoDB', err));
-
-
-
-  app.get('/usermessages',tokens, usermessages);
-  app.get('/allmessages',tokens, allusermessages);
-  app.get('/getsingleusrchat/:name',tokens, getsingleusrchat);
-
-
 
   httpServer.listen(port, () => console.log(`listening on port ${port}`));
--- controllers/chat.controller.js
+++ controllers/chat.controller.js
@@ -6,50 +6,8 @@ module.exports={
             sender:sender,
             chatdata:data
         })
-
         const saved= await save.save()
-    },
-    usermessages:async (req, res) => {
-        console.log(req.tokens);
-        console.log('manyu');
-        try {
-            const messages = await chats.find( {
-              $or: [
-                { sender: req.tokens.username },
-                { reciever: req.tokens.username }
-            ]
-            })
-            res.json(messages);
-        } catch (error) {
-            console.error('Error fetching messages:', error);
-            res.status(500).json({ error: 'Internal server error' });
-        }
-    },
-    allusermessages:async (req, res) => {
-        console.log(req.tokens);
-        console.log('manyu');
-        try {
-            const messages = await chats.find({})
-            res.json(messages);
-        } catch (error) {
-            console.error('Error fetching messages:', error);
-            res.status(500).json({ error: 'Internal server error' });
-        }
-    },
-    getsingleusrchat:async (req, res) => {
-        try {
-            const username= req.params.name
-            const messages = await chats.find({
-                $or: [
-                    { sender:username },
-                    { reciever:username }
-                ]
-            })
-            console.log(messages);
-            res.json(messages);
-        } catch (error) {
-            console.error('Error fetching messages:', error);
-            res.status(500).json({ error: 'Internal server error' });
-        }
+
+        console.log(saved);
     },
 }
